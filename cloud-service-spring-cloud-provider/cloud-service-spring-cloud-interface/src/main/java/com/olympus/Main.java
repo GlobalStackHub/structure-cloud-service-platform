03b3fade68a3a5f91e9ea63d7211118c6c60953b
eddie.lys <ysh.li@misumi.sh.cn>
2022-11-24 11:07:28 +0800

    init repo

---
+++ cloud-service-spring-cloud-provider/cloud-service-spring-cloud-interface/src/main/java/com/olympus/Main.java
@@ -0,0 +1,13 @@
+package com.olympus;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author eddie.lys
+ * @since ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
