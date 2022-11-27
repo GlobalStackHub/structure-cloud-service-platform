a45284a77a41f597d16a99a47e80daea24f5d1a7
eddie.lys <ysh.li@misumi.sh.cn>
2022-11-27 20:26:52 +0800

    update: kubernetes file

--- cloud-service-center-domain-router/src/main/java/com/olympus/router/CloudServiceCenterDomainRouterApplication.java
+++ cloud-service-center-domain-router/src/main/java/com/olympus/router/CloudServiceCenterDomainRouterApplication.java
@@ -2,12 +2,24 @@ package com.olympus.router;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CloudServiceCenterDomainRouterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudServiceCenterDomainRouterApplication.class, args);
+    }
+
+    @GetMapping("/application/alive/check")
+    public String aliveCheck() {
+        return "200";
     }
 
 }
